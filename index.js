f40c033ba353cd6a3ca7cb9e97b979f54ec5ff14
MACBOOK <macbook@MACBOOKs-MacBook-Pro-2.local>
2021-11-22 22:08:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,124 @@
+let app;
+app = new Vue({
+  el: "#app",
+  data: {
+    form: {
+      name: "",
+      phone: "",
+    },
+    cartItems: [],
+    orderBy: "Ascending",
+    sortBy: "Title",
+    search: "",
+    currentPage: "checkout",
+    sortByPlaceHolder: ["location", "price", "spaces", "rating", "title"],
+    orderByPlaceholder: ["ascending", "descending"],
+    subjects: data,
+  },
+  methods: {
+    navigate(id) {
+      if (id && typeof id === "string") return (this.currentPage = id);
+      const page = this.currentPage === "home" ? "cart" : "home";
+      this.currentPage = page;
+    },
+    removeFromCart(id) {
+      console.log({ id });
+      const arr = this.cartItems;
+      var index = arr.findIndex((e) => e === id);
+      if (index > -1) {
+        arr.splice(index, 1);
+      }
+      this.cartItems = arr;
+    },
+    getCartProducts() {
+      const sub = this.subjects;
+      const products = this.cartItems.reduce((acc, curr) => {
+        if (acc[`id-${curr}`]) {
+          acc[`id-${curr}`] = {
+            ...acc[`id-${curr}`],
+            inCart: acc[`id-${curr}`].inCart + 1,
+          };
+          return acc;
+        }
+        const find = sub.find((e) => e.id == curr);
+        acc[`id-${curr}`] = {
+          ...find,
+          inCart: 1,
+        };
+        return acc;
+      }, {});
+      return Object.values(products);
+    },
+    addToCart(e) {
+      const cart = app.cartItems;
+      cart.push(e);
+    },
+    itemsInCart(id) {
+      return this?.cartItems.filter((e) => e === id).length;
+    },
+    isEnabled(id) {
+      const subject = this.subjects.find((e) => e.id === id);
+      const getItemsInCart = this.itemsInCart(subject.id);
+      console.log(subject.spaces > getItemsInCart);
+      return subject.spaces > getItemsInCart;
+    },
+  },
+  watch: {
+    search(val) {
+      if (val === "") return (this.subjects = data);
+      this.subjects = this.subjects.filter((e) =>
+        e.title.toLowerCase().includes(val.toLowerCase())
+      );
+    },
+    orderBy(val) {
+      const { subjects, sortBy } = this;
+      const n = val === "ascending" ? -1 : 1;
+      const p = val === "ascending" ? 1 : -1;
+      const sub = subjects.sort(function (a, b) {
+        if (a[sortBy] < b[sortBy]) {
+          return n;
+        }
+        if (a[sortBy] > b[sortBy]) {
+          return p;
+        }
+        return 0;
+      });
+      console.log({ subjects });
+
+      this.subjects = sub;
+    },
+
+    sortBy(sortBy) {
+      const { subjects, orderBy } = this;
+      const n = orderBy === "ascending" ? -1 : 1;
+      const p = orderBy === "ascending" ? 1 : -1;
+      const sub = subjects.sort(function (a, b) {
+        if (a[sortBy] < b[sortBy]) {
+          return n;
+        }
+        if (a[sortBy] > b[sortBy]) {
+          return p;
+        }
+        return 0;
+      });
+      console.log({ subjects });
+
+      this.subjects = sub;
+    },
+  },
+  computed: {
+    pageInverse() {
+      const page = this.currentPage === "home" ? "cart" : "home";
+      return page;
+    },
+    totalCartSum() {
+      return this.getCartProducts().reduce((acc, curr) => {
+        acc += curr.price * curr.inCart;
+        return acc;
+      }, 0);
+    },
+    cartItemCount() {
+      return this.cartItems.length;
+    },
+  },
+});
---
+++ product.js
@@ -0,0 +1,59 @@
+const data = [
+  {
+    id: 1,
+    image:
+      "https://thumbs.dreamstime.com/b/vector-artistic-drawing-illustration-old-closed-history-book-pen-ink-119233665.jpg",
+    location: 2,
+    price: 24,
+    spaces: 3,
+    title: "History",
+    rating: 4,
+  },
+  {
+    id: 2,
+    image: "https://pma.com.pk/wp-content/uploads/2020/02/9789695052174.jpg",
+    location: 3,
+    price: 60,
+    spaces: 5,
+    title: "Mathematics Advance",
+    rating: 5,
+  },
+  {
+    id: 3,
+    image:
+      "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781638356097/vue-js-in-action-9781638356097_hr.jpg",
+    location: 3,
+    price: 100,
+    spaces: 5,
+    title: "VueJS In Action",
+    rating: 2,
+  },
+  {
+    id: 4,
+    image:
+      "https://www.expresspublishing.co.uk/assets/microsites/adv/images/book-image-3D.png",
+    location: 14,
+    price: 24,
+    spaces: 2,
+    title: "Advance Computer Science",
+    rating: 5,
+  },
+  {
+    id: 5,
+    image: "https://www.golang-book.com/public/img/introducing-go/cover.jpg",
+    location: 35,
+    price: 54,
+    spaces: 3,
+    title: "Go Lang - Introduction",
+    rating: 5,
+  },
+  {
+    id: 6,
+    image: "https://miro.medium.com/max/1131/1*UujExOodICsNuRHL-7v9Yw.jpeg",
+    location: 2,
+    price: 24,
+    spaces: 2,
+    title: "NODEJS - Advance",
+    rating: 3,
+  },
+];
